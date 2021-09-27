7fc95ed3310a851efa3a393d6ef76fde37873718
preminstrel <sunhanshi1225@163.com>
2021-09-27 17:38:17 +0800

    FINISH

--- quant/__init__.py
+++
@@ -1,8 +0,0 @@
-# This file is part of PWLQ repository.
-# Copyright (c) Samsung Semiconductor, Inc.
-# All rights reserved.
-
-from .pwlq import *
-from .uniform import *
-from .quant_acts import *
-from .quant_weis import *
